6b8e4f96e2ecc96c38e330cf608d1999e4bf076c
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-13 09:59:05 +0100

    1b-01fx

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 #increase hard and soft limit fixToo Many Open Files Error
 
 exec { 'soft-limit':
-  command => 'sed -i "holberton soft/s/5/10000/" /etc/security/limits.conf,
+  command => 'sed -i "holberton soft/s/4/10000/" /etc/security/limits.conf,
   path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'hard-limit':
-  command => 'sed -i "holberton hard/s/4/30000/" /etc/security/limits.conf',
+  command => 'sed -i "holberton hard/s/5/30000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
