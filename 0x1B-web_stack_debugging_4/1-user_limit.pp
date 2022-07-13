1da5d7203d3119f855e2116924715bca43a5f4db
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-13 10:07:01 +0100

    1b-01fx

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 #increase hard and soft limit fixToo Many Open Files Error
 
 exec { 'soft-limit':
-  command => 'sed -i "nginx       soft    holberton   10000" /etc/security/limits.conf',
+  command => 'echo "nginx       soft    holberton   10000" > /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'hard-limit':
-  command => 'sed -i "nginx       hard    holberton  30000" /etc/security/limits.conf',
+  command => 'echo "nginx       hard    holberton  30000" > /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
