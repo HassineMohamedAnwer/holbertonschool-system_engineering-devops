eb626e41a43fcdbe072621911cee4c918fa4feae
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-13 09:39:57 +0100

    1b-00fx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 #update requests limit nginx
 exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf",
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
 
