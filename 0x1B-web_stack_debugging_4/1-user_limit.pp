cbf9a64004aeeee539ea32f9f34dea7b0bc2f2f7
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-13 09:52:34 +0100

    1b-00fx

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,6 +3,7 @@ exec { 'soft-limit':
   command => 'sed -i "/holberton soft/s/5/10000/" /etc/security/limits.conf,
   path    => '/usr/local/bin/:/bin/'
 }
+
 exec { 'hard-limit':
   command => 'sed -i "/holberton hard/s/4/30000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
